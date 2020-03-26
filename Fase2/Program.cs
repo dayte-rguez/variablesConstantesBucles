6464cfb3db84780d06f15efdf2e6b52b192f68ff
dayte <dayte.rodriguez@gmail.com>
2020-03-26 12:18:20 +0100

    Created constants containing the first leap year for the exercise (1948) and the leap years period.

---
+++ Fase2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    static class Constants
+    {
+        //Create a constant containing the leap year 1948
+        public const int firstLeapYear = 1948;
+
+        //Create a constant with the period needed for a new leap year
+        public const int newLeapYear = 4;
+    }
+}
