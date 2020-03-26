bd9a63632f437cf7010b92d5b393c150b7ce57af
dayte <dayte.rodriguez@gmail.com>
2020-03-26 13:56:28 +0100

    Shows the leap years between 1948 and the birthyear

---
+++ Fase3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Show the leap years between 1948 and the birthyear
+            for (var i = Constants.firstLeapYear; i <= 1983; i += Constants.newLeapYear)
+            {
+                Console.WriteLine($"{i}");
+            }
+            
+        }
+    }
+    static class Constants
+    {
+        public const int firstLeapYear = 1948;
+        public const int newLeapYear = 4;
+    }
+}
