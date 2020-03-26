e30edc8205749b1c430ae9e6053d752f16328460
dayte <dayte.rodriguez@gmail.com>
2020-03-26 11:59:14 +0100

    Created variables for name and last name

---
+++ Fase1/Fase1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Dayté Dayana";
+            var firtsLastname = "Rodríguez";
+            var secondLastname = "Martínez";
+        }
+    }
+}
