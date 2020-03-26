79f4611208c1a59b0bc915e84ad810fc86b2d891
dayte <dayte.rodriguez@gmail.com>
2020-03-26 16:09:35 +0100

    Shows if the birthyear is leap

--- Fase3/Program.cs
+++ Fase3/Program.cs
@@ -6,17 +6,31 @@ namespace Fase3
     {
         static void Main(string[] args)
         {
+            int i;
             //Show the leap years between 1948 and the birthyear
-            for (var i = Constants.firstLeapYear; i <= 1983; i += Constants.newLeapYear)
+            for (i = Constants.firstLeapYear; i <= Constants.birthYear; i += Constants.newLeapYear)
             {
                 Console.WriteLine($"{i}");
             }
-            
+
+            //Show if the birthyear is leap
+            var leapBirthYear = false;
+            if (i == Constants.birthYear + Constants.newLeapYear)
+            {
+                leapBirthYear = !leapBirthYear;
+                Console.WriteLine($"The birthyear {Constants.birthYear} is leap");
+            }
+            else
+            {
+                Console.WriteLine($"The birthyear {Constants.birthYear} is not leap");
+            }
+
         }
     }
     static class Constants
     {
         public const int firstLeapYear = 1948;
         public const int newLeapYear = 4;
+        public const int birthYear = 1983;
     }
 }
